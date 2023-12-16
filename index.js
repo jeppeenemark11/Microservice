3ab3a22603c6ca74fa9d1f1500adf0795eb033d8
jeppeenemark11 <148648119+jeppeenemark11@users.noreply.github.com>
2023-12-16 08:56:55 +0100

    Update1 index.js

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/:whoami', function (req, res) {
-  res.json({ "ipaddress": req.ip, "language": req.headers["accept-language"], "software": req.headers[user-agent] });
+app.get('/api/whoami', function (req, res) {
+  
+  res.json({ "ipaddress": req.ip, "language": req.headers["accept-language"], "software": req.headers["user-agent"] });
 });
 
 
